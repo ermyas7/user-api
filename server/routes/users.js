e9390c68bf15fbf0ebfef8bc5f376d0dcc31c044
ermyas7 <ermyas.fekadu@gmail.com>
2019-03-29 16:10:54 +0530

    refactor user route

---
+++ server/routes/users.js
@@ -0,0 +1,78 @@
+const express = require('express');
+
+const {
+    User
+} = require('../models/user');
+const {
+    authenticate
+} = require('../middleware/authenticate');
+const _ = require('lodash');
+
+const router = express.Router({
+    mergeParams: true
+});
+
+/////////////////////////
+//register user
+////////////////////////
+
+router.post('/users', (req, res) => {
+    let body = _.pick(req.body, ['email', 'username', 'password'])
+    var user = new User(body);
+    user.save()
+        .then(() => {
+            return user.generateAuthToken();
+        })
+        .then((token) => {
+            res.header('x-auth', token).send({
+                user
+            })
+        })
+        .catch(err => {
+            res.status(400).send({
+                error: err.message
+            })
+        })
+})
+
+//////////////////////////////////////////////
+///////////////login user route  ////////////
+////////////////////////////////////////////
+
+router.post('/users/login', (req, res) => {
+
+    //pick password and email from request data
+    var body = _.pick(req.body, ['email', 'password'])
+
+    //authenticate the given user using credentials
+    User.findByCredentials(body.email, body.password)
+        .then(user => {
+            return user.generateAuthToken()
+                .then((token) => {
+                    res.header('x-auth', token).send({
+                        user
+                    })
+                })
+        })
+        .catch(err => {
+            res.status(400).send({
+                error: err
+            })
+        })
+})
+
+//////////////////////////////////////////////
+///////////////logout user route  ////////////
+////////////////////////////////////////////
+
+router.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token)
+        .then(() => {
+            res.status(200).send();
+        }, () => {
+            res.status(400).send()
+        })
+})
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,18 +1,26 @@
 require('./config/config')
-const _ 		 = require('lodash')
-const mongoose   = require('./db/mongoose')
-const express    = require('express')
+const _ = require('lodash')
+const mongoose = require('./db/mongoose')
+const express = require('express')
 const bodyParser = require('body-parser')
 const cloudinary = require('cloudinary').v2
-const multer     = require('multer')
+const multer = require('multer')
 
-
-var {User}   = require('./models/user');
-var {Movie} = require('./models/movie');
-var {authenticate} = require('./middleware/authenticate');
+const usersRouter = require('./routes/users');
+const {
+  User
+} = require('./models/user');
+const {
+  Movie
+} = require('./models/movie');
+const {
+  authenticate
+} = require('./middleware/authenticate');
 const PORT = process.env.PORT;
 
-const {seedMovie} = require('./seed');
+const {
+  seedMovie
+} = require('./seed');
 
 //seedMovie();
 
@@ -21,150 +29,111 @@ const app = express();
 app.use(bodyParser.json());
 
 
-app.get('/', (req,res) => res.send("hello world!"));
+app.get('/', (req, res) => res.send("hello world!"));
 
-/////////////////////////
-//register user
-////////////////////////
 
-app.post('/users', (req, res) => {
-  let body = _.pick(req.body, ['email','username','password'])
-  var user = new User(body);
-  user.save()
-  .then(() => {
-    return user.generateAuthToken();
-  })
-  .then((token) => {
-    res.header('x-auth',token).send({user})
-  })
-  .catch(err => {
-    res.status(400).send({error: err.message})
-  })
-})
 
-  //////////////////////////////////////////////
-  ///////////////login user route  ////////////
-  ////////////////////////////////////////////
 
-  app.post('/users/login', (req, res) => {
 
-    //pick password and email from request data
-    var body = _.pick(req.body, ['email', 'password'])
+////////////////////////////////////////////////
+///////////////////////////////////////////////
+/////// movies route //////////////////////////
+//////////////////////////////////////////////
+/////////////////////////////////////////////
 
-    //authenticate the given user using credentials
-    User.findByCredentials(body.email, body.password)
-    .then(user => {
-       return user.generateAuthToken()
-       .then((token) => {
-         res.header('x-auth',token).send({user})
-       })
+
+//////////////////////////////////////
+////// get all movies ////////////////
+/////////////////////////////////////
+app.get('/movies', (req, res) => {
+  Movie.find({})
+    .then(movies => {
+      res.status(200).send(movies)
     })
     .catch(err => {
-      res.status(400).send({error: err})
+      res.status(400).send(err)
     })
-  })
+});
 
-  //////////////////////////////////////////////
-  ///////////////logout user route  ////////////
-  ////////////////////////////////////////////
 
-  app.delete('/users/me/token',authenticate, (req, res) => {
-    req.user.removeToken(req.token)
-    .then(() =>{
-      res.status(200).send();
-    }, () => {
-      res.status(400).send()
+////////////////////////////////
+///////// get one movie ////////
+//////////////////////////////
+app.get('/movies/:id', (req, res) => {
+  let id = req.params.id;
+  Movie.findById(id)
+    .then(movie => {
+      res.status(200).send(movie)
     })
-  })
-
-
-
-  ////////////////////////////////////////////////
-  ///////////////////////////////////////////////
-  /////// movies route //////////////////////////
-  //////////////////////////////////////////////
-  /////////////////////////////////////////////
-
-
-  //////////////////////////////////////
-  ////// get all movies ////////////////
-  /////////////////////////////////////
-  app.get('/movies', (req, res) => {
-  	Movie.find({})
-  	.then(movies => {
-  		res.status(200).send(movies)
-  	})
-  	.catch(err => {
-  		res.status(400).send(err)
-  	})
-  });
-
-
-  ////////////////////////////////
-  ///////// get one movie ////////
-  //////////////////////////////
-  app.get('/movies/:id', (req, res) => {
-  	let id = req.params.id;
-  	Movie.findById(id)
-  	.then(movie => {
-  		res.status(200).send(movie)
-  	})
-  	.catch(err => {
-  		res.status(400).send(err)
-  	})
-  });
+    .catch(err => {
+      res.status(400).send(err)
+    })
+});
 
 
 
 
-  ///////////////////////////////////////////////////
-  ///////////////////////////////////////////////////
-  ///////cloudinary configuration ///////////////////
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+///////cloudinary configuration ///////////////////
 
-  var storage = multer.diskStorage({
-  	filename: function(req, file, callback) {
-    	callback(null, Date.now() + file.originalname);
-  	}
+var storage = multer.diskStorage({
+  filename: function (req, file, callback) {
+    callback(null, Date.now() + file.originalname);
+  }
 });
 
 var imageFilter = function (req, file, cb) {
-    // accept image files only
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
-        return cb(new Error('Only image files are allowed!'), false);
-    }
-    cb(null, true);
+  // accept image files only
+  if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/i)) {
+    return cb(new Error('Only image files are allowed!'), false);
+  }
+  cb(null, true);
 };
 
-var upload = multer({ storage: storage, fileFilter: imageFilter, limits:{fileSize: 1000000}})
+var upload = multer({
+  storage: storage,
+  fileFilter: imageFilter,
+  limits: {
+    fileSize: 1000000
+  }
+})
 
 
-cloudinary.config({ 
-  cloud_name: 'djinwsqhr', 
-  api_key: process.env.CLOUDINARY_API_KEY, 
+cloudinary.config({
+  cloud_name: 'djinwsqhr',
+  api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 app.post('/movies', upload.single('image'), (req, res) => {
-	//upload file to cloudinary server
-	cloudinary.uploader.upload(req.file.path, (err, result) => {
- 	if(err){
-	 		console.log("error from cloudinary")
+  //upload file to cloudinary server
+  cloudinary.uploader.upload(req.file.path, (err, result) => {
+    if (err) {
+      console.log("error from cloudinary")
 
- 	}	
-  	// Grab image url
-  	const Poster = result.secure_url;
-  	
-  	//movie list object
-  	let body = _.pick(req.body, ['Title', 'Year', 'Type']);
-  		body = {...body, Poster};
-  		
-  	//add movie list object to BD
-  	Movie.create(body)
-  	.then(movie => res.status(200).send(movie))
-  	.catch(err => {
-  		console.log("error from movie"); 
-  		res.status(400).send(err)});
-	});
+    }
+    // Grab image url
+    const Poster = result.secure_url;
+
+    //movie list object
+    let body = _.pick(req.body, ['Title', 'Year', 'Type']);
+    body = {
+      ...body,
+      Poster
+    };
+
+    //add movie list object to BD
+    Movie.create(body)
+      .then(movie => res.status(200).send(movie))
+      .catch(err => {
+        console.log("error from movie");
+        res.status(400).send(err)
+      });
+  });
 })
+
+//add external routers to app
+app.use("/", usersRouter);
 
 app.listen(PORT, () => console.log(`server running on port ${PORT}`));
